ce7549359d156054ba55455107fd60eb80203cbe
Jake Rozsa <jakebrozsa@gmail.com>
2019-03-11 09:12:57 -0600

    Still Buggy

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath: '/bug-log/',
+  outputDir: 'docs',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
